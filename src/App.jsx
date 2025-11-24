af79d0c313df899bbdf99489ea56db37f22b2c52
huw <huw.llewellyn@gmail.com>
2025-11-24 09:47:34 +0000

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+// Import section components (to be created)
+// import IntroSection from './components/IntroSection'
+// import VizSection from './components/VizSection'
+// import ConclusionSection from './components/ConclusionSection'
+
+function App() {
+  return (
+    <>
+      {/* <IntroSection /> */}
+      <div>Intro Section (to be implemented)</div>
+
+      {/* <VizSection /> */}
+      <div>Viz Section (to be implemented)</div>
+
+      {/* <ConclusionSection /> */}
+      <div>Conclusion Section (to be implemented)</div>
+    </>
+  )
+}
+
+export default App
