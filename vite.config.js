83a54c22e2a7c333f12e5e629a6026814e7842df
huw <huw.llewellyn@gmail.com>
2025-11-25 13:55:17 +0000

    try different source for github

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/ft-article-axa/' : '/',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'serve' ? '/' : '/ft-article-axa/',
+  }
 })
